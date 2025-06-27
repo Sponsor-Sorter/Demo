a7dca77583b00bbc6bccff8d08b62a2644d4e3a4
Sponsor-Sorter <jrb.williams.95@gmail.com>
2025-06-28 00:52:33 +1000

    Update homepage.js
    ./ update

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,5 @@
 // /public/js/homepage.js
-import { supabase } from '/public/js/supabaseClient.js';
+import { supabase } from './supabaseClient.js';
 
 /* ========== ACTIVITY TICKER ========== */
 function timeAgo(dateString) {
